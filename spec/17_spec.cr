37380a554f2a0b585509cb7de569245c4f8cd535
Greg Woods <gregwoods01@gmail.com>
2024-12-17 09:29:51 -0500

    17 part 1

--- spec/17_spec.cr
+++ spec/17_spec.cr
@@ -4,6 +4,54 @@ describe Day17 do
   day = Day17.new
   input = Input.from_example("17.txt")
 
+  describe "perform_operation" do
+    it "performs operation 2" do
+      day.register_c = 9
+      day.perform_operation(2, 6)
+      day.register_b.should eq(1)
+    end
+
+    it "performs a series of operations" do
+      program = <<-STRING
+      Register A: 10
+      Register B: 0
+      Register C: 0
+
+      Program: 5,0,5,1,5,4
+      STRING
+
+      day.part1(Input.new(program))
+      day.output.should eq([0,1,2])
+    end
+
+    it "performs a series of operations" do
+      program = <<-STRING
+      Register A: 2024
+      Register B: 0
+      Register C: 0
+
+      Program: 0,1,5,4,3,0
+      STRING
+
+      day.part1(Input.new(program))
+      day.output.should eq([4,2,5,6,7,7,7,7,3,1,0])
+      day.register_a.should eq(0)
+    end
+
+    it "performs a series of operations" do
+      program = <<-STRING
+      Register A: 0
+      Register B: 29
+      Register C: 0
+
+      Program: 1,7
+      STRING
+
+      day.part1(Input.new(program))
+      day.register_b.should eq(26)
+    end
+  end
+
   describe "part1" do
     it "works" do
       day.part1(input).should eq("4,6,3,5,6,3,5,2,1,0")
--- src/days/17.cr
+++ src/days/17.cr
@@ -1,4 +1,6 @@
 class Day17 < Day
+  property register_a, register_b, register_c, output
+
   def initialize
     @register_a = 0_i64
     @register_b = 0_i64
@@ -6,7 +8,14 @@ class Day17 < Day
     @output = [] of Int64
   end
 
-  def parse(input) : Array(Int64)
+  # def reset!
+  #   @register_a = 0_i64
+  #   @register_b = 0_i64
+  #   @register_c = 0_i64
+  #   @output = [] of Int64
+  # end
+
+  def parse(input) : Array(Int32)
     lines = input.lines
 
     @output.clear
@@ -14,7 +23,7 @@ class Day17 < Day
     @register_b = lines[1].split(": ").last.to_i64
     @register_c = lines[2].split(": ").last.to_i64
 
-    lines[4].split(": ").last.split(",").map(&.to_i64)
+    lines[4].split(": ").last.split(",").map(&.to_i)
   end
 
   private def combo_operand_value(input)
@@ -38,31 +47,24 @@ class Day17 < Day
   #   end
   # end
 
-  def perform_operation(opcode, operand) : Int64?
+  def perform_operation(opcode, operand) : Int32?
     case opcode
     when 0
       @register_a = (@register_a / (2 ** combo_operand_value(operand))).to_i64
     when 1
-      @register_b = @register_b | operand
+      @register_b = @register_b ^ operand
     when 2
-      combo_operand_value(operand) % 8
+      @register_b = (combo_operand_value(operand) % 8).to_i64
     when 3
-      puts "Jump instruction!"
-        if @register_a != 0
-          puts "A register is #{@register_a}, doing jump"
-          return operand
-          # return Result.new(operand, nil)
-        else
-          puts "A register is #{@register_a}, not jumping"
-        end
+      return operand if @register_a != 0
     when 4
-      @register_b = @register_b | @register_c
+      @register_b = @register_b ^ @register_c
     when 5
-      @output << combo_operand_value(operand) % 8
+      @output << (combo_operand_value(operand) % 8).to_i64
     when 6
-      @register_b = (@register_a / (combo_operand_value(operand) ** 2)).to_i64
+      @register_b = (@register_a / (2 ** combo_operand_value(operand))).to_i64
     when 7
-      @register_c = (@register_a / (combo_operand_value(operand) ** 2)).to_i64
+      @register_c = (@register_a / (2 ** combo_operand_value(operand))).to_i64
     else
       raise "Invalid opcode value #{opcode}!"
     end
@@ -74,21 +76,14 @@ class Day17 < Day
     instructions = parse(input)
     output = [] of Int64
     position = 0
-    iterations = 0
 
     loop do
-      iterations += 1
-      if iterations > 10_000
-        puts "Giving up"
-        exit
-      end
-
       if position >= (instructions.size - 1)
         break # halt program!
       end
 
-      puts ""
-      puts "Performing #{instructions[position]} code with #{instructions[position + 1]} operand"
+      # puts ""
+      # puts "Performing #{instructions[position]} code with #{instructions[position + 1]} operand"
 
       result = perform_operation(
         instructions[position],
@@ -97,17 +92,17 @@ class Day17 < Day
 
       if result
         position = result
-        puts "Jumped to position #{position}"
+        # puts "Jumped to position #{position}"
       else
         position += 2
-        puts "Moving to position #{position}"
+        # puts "Moving to position #{position}"
       end
 
-      puts "Resister status:"
-      puts "A: #{@register_a}"
-      puts "B: #{@register_b}"
-      puts "C: #{@register_c}"
-      puts "Current output: #{output}"
+      # puts "Resister status:"
+      # puts "A: #{@register_a}"
+      # puts "B: #{@register_b}"
+      # puts "C: #{@register_c}"
+      # puts "Current output: #{output}"
     end
 
     @output.join(",")
