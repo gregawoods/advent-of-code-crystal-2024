0e75bed11af00af876c3f4b1dcad37dab10e9d8c
Greg Woods <gregwoods01@gmail.com>
2024-12-22 19:02:41 -0500

    21 wip

--- src/days/21.cr
+++ src/days/21.cr
@@ -1,50 +1,50 @@
 class Day21 < Day
   property sequence
 
-  alias Point = {Int32, Int32}
+  alias Point = {Int128, Int128}
 
   def initialize
     # # This is the FIRST keypad position that we directly control, which controls the first robot
     # @keypad_position_a = {2, 3}
     #
-    @numeric_keypad_robot_position = {2, 3}
+    @numeric_keypad_robot_position = {2_i128, 3_i128}
 
     # This tracks the posiotiosn of directional robots
     @directional_robot_positions = Array(Point).new
 
-    @cache = Hash(String, Int32).new
+    @cache = Hash(String, Int128).new
   end
 
   DOOR_BUTTON_POSITIONS = {
-    '7' => {0, 0},
-    '8' => {1, 0},
-    '9' => {2, 0},
-    '4' => {0, 1},
-    '5' => {1, 1},
-    '6' => {2, 1},
-    '1' => {0, 2},
-    '2' => {1, 2},
-    '3' => {2, 2},
-    'x' => {0, 3},
-    '0' => {1, 3},
-    'A' => {2, 3}
-  }
+    '7' => {0_i128, 0_i128},
+    '8' => {1_i128, 0_i128},
+    '9' => {2_i128, 0_i128},
+    '4' => {0_i128, 1_i128},
+    '5' => {1_i128, 1_i128},
+    '6' => {2_i128, 1_i128},
+    '1' => {0_i128, 2_i128},
+    '2' => {1_i128, 2_i128},
+    '3' => {2_i128, 2_i128},
+    'x' => {0_i128, 3_i128},
+    '0' => {1_i128, 3_i128},
+    'A' => {2_i128, 3_i128}
+  } of Char => Point
 
   CONTROL_BUTTON_POSITIONS = {
-    'x' => {0, 0},
-    '^' => {1, 0},
-    'A' => {2, 0},
-    '<' => {0, 1},
-    'v' => {1, 1},
-    '>' => {2, 1}
-  }
+    'x' => {0_i128, 0_i128},
+    '^' => {1_i128, 0_i128},
+    'A' => {2_i128, 0_i128},
+    '<' => {0_i128, 1_i128},
+    'v' => {1_i128, 1_i128},
+    '>' => {2_i128, 1_i128}
+  } of Char => Point
 
-  def keypad_robot_press(dest : Char, max_depth : Int32) : Int32
+  def keypad_robot_press(dest : Char, max_depth : Int128) : Int128
     dest_point = DOOR_BUTTON_POSITIONS[dest]
     dx = dest_point[0] - @numeric_keypad_robot_position[0]
     dy = dest_point[1] - @numeric_keypad_robot_position[1]
 
-    sequences = [] of Int32
+    sequences = [] of Int128
 
     # check to see if we can move horizontally first
     if DOOR_BUTTON_POSITIONS['x'] != { @numeric_keypad_robot_position[0] + dx, @numeric_keypad_robot_position[1] }
@@ -59,10 +59,9 @@ class Day21 < Day
       end
       moves += inner_robot_press('A', 0, max_depth)
       sequences << moves
-    end
 
     # check to see if we can move vertically first
-    if DOOR_BUTTON_POSITIONS['x'] != { @numeric_keypad_robot_position[0], @numeric_keypad_robot_position[1] + dy }
+    elsif DOOR_BUTTON_POSITIONS['x'] != { @numeric_keypad_robot_position[0], @numeric_keypad_robot_position[1] + dy }
       moves = 0
       dir = dy.positive? ? 'v' : '^'
       dy.abs.times do
@@ -80,11 +79,7 @@ class Day21 < Day
     sequences.min
   end
 
-  def inner_robot_press(dest : Char, depth : Int32, max_depth : Int32) : Int32
-    # if @directional_robot_positions.size < depth + 1
-    #   @directional_robot_positions << {2, 0}
-    # end
-
+  def inner_robot_press(dest : Char, depth : Int128, max_depth : Int128) : Int128
     dest_point = CONTROL_BUTTON_POSITIONS[dest]
     dx = dest_point[0] - @directional_robot_positions[depth][0]
     dy = dest_point[1] - @directional_robot_positions[depth][1]
@@ -98,7 +93,7 @@ class Day21 < Day
       return @cache[cache_key]
     end
 
-    sequences = [] of Int32
+    sequences = [] of Int128
 
     if depth == max_depth
       sequences << dx.abs + dy.abs + 1
@@ -116,10 +111,9 @@ class Day21 < Day
         end
         moves += inner_robot_press('A', depth + 1, max_depth)
         sequences << moves
-      end
 
       # check to see if we can move vertically first
-      if CONTROL_BUTTON_POSITIONS['x'] != { @directional_robot_positions[depth][0], @directional_robot_positions[depth][1] + dy }
+      elsif CONTROL_BUTTON_POSITIONS['x'] != { @directional_robot_positions[depth][0], @directional_robot_positions[depth][1] + dy }
         moves = 0
         dir = dy.positive? ? 'v' : '^'
         dy.abs.times do
@@ -140,9 +134,9 @@ class Day21 < Day
     @cache[cache_key]
   end
 
-  def calculate_total_moves(sequence : String, max_depth : Int32) : Int32
+  def calculate_total_moves(sequence : String, max_depth : Int128) : Int128
     @directional_robot_positions = (0..max_depth).map do
-      {2, 0}
+      {2_i128, 0_i128}
     end
 
     sequence.chars.sum do |char|
@@ -158,9 +152,9 @@ class Day21 < Day
   end
 
   def part2(input)
-    input.lines.sum do |line|
+    input.lines.each do |line|
       puts "\n\nLine #{line}"
-      line[0..2].to_i * calculate_total_moves(line, 24)
+      puts line[0..2].to_i * calculate_total_moves(line, 24)
     end
   end
 end
